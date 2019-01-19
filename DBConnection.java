32e23b259ce9dc05be5a0ea39253db459f2e6204
justbojar <46721258+justbojar@users.noreply.github.com>
2019-01-19 15:48:15 +0100

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,27 @@
+package trams;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ * Klasa sluzaca do utworzenia polaczenia JDBC z baza Oracle
+ * @author Justyna
+ */
+
+public class DBConnection {
+	
+	private static final String DB_URL = "jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf";
+	
+	public static Connection getConnection(){
+		Connection conn = null;
+		try{
+		//Utworzenie polaczenia
+		conn = DriverManager.getConnection(DB_URL, "pkulig", "pkulig");
+		if (conn != null){
+		//	JOptionPane.showMessageDialog(null, "Utworzono polaczenie z baza danych.");
+		}
+		}catch(SQLException exc){
+			JOptionPane.showMessageDialog(null, "Problem z nawiazaniem polaczenia z baza danych. " + exc.getMessage(), "Errors", JOptionPane.ERROR_MESSAGE);
+		}
+		return conn;
+	}
+}
